c0f352804d66cd4388b50e57caa5268f167d4b6b
Seiya Kawamot <kid960805@gmail.com>
2020-03-19 19:37:45 +0900

    メール機能の追加

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -12,7 +12,8 @@ class ContactsController extends Controller
     public function index(){
         return view('contacts.index');
     }
-
+    
+    //入力内容確認を保存し、確認画面へ
     public function confirm(ContactRequest $request){
         $inputs = new Contact();
 
@@ -28,18 +29,27 @@ class ContactsController extends Controller
         ]);
     }
 
+    //入力内容の送信
     public function complete(ContactRequest $request){
+
+        //フォームから受け取ったactionを除いたinputの値を取得
         $action = $request->input('action');
 
+        //フォームから受け取ったactionを除いたinputの値を取得
         $inputs = $request->except('action');
 
+        //actionの値で分岐する
         if($action !== 'submit'){
             return redirect()->route('contact.index')->withInput($inputs);
         }else{
+            //入力されたメールアドレスにメールを送信
+            //送信先を指定して、mailableクラスでの処理による内容を送信する。
             \Mail::to($inputs['email'])->send(new ContactSendmail($inputs));
 
+            //再送信を防ぐためにトークンを再発行
             $request->session()->regenerateToken();
 
+            //送信完了のページを表示
             return view('contacts.complete');
         }
     }
